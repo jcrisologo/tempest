a9064db2050467c7e70a3430b800507de113c32c
jcrisologo <jcrisologo@users.noreply.github.com>
2019-03-12 23:20:29 -0700

    it fucking works!!!!

--- player.cpp
+++ player.cpp
@@ -25,56 +25,45 @@
 #include <SDL.h>
 #include <string.h>
 
-double carrier;
+double fc;
 int resx;
 int resy;
 int horizontalspan;
+int verticalspan;
 double pixelclock;
 
 SDL_Window* sdlWindow;
 SDL_Renderer* sdlRenderer;
-SDL_Texture* send0;
-SDL_Texture* send1;
+SDL_Texture* send;
 SDL_Surface* screen;
 uint32_t* pixbuff;
+uint8_t* audiobuff;
+float* carrier;
+int audiolength;
 
 void play ()
 {
-unsigned int data[] = {0, 0, 0, 1,
-	                 0, 0, 1, 0,
-			 0, 0, 1, 1,
-			 0, 1, 0, 0,
-			 0, 1, 0, 1,
-			 0, 1, 1, 0,
-			 0, 1, 1, 1,
-			 1, 0, 0, 0,
-			 1, 0, 0, 1,
-			 1, 0, 1, 0,
-			 1, 0, 1, 1,
-			 1, 1, 0, 0,
-			 1, 1, 0, 1,
-			 1, 1, 1, 0,
-			 1, 1, 1, 1,};
-
-  size_t data_len = sizeof(data)/sizeof(data[0]);
   int pos=0;
   while (1) {
     SDL_Event event;
     while(SDL_PollEvent(&event)) if (event.type == SDL_MOUSEBUTTONDOWN) exit(0);
 
-       if (pos >= data_len) pos = 0;
+       if (pos >= audiolength) pos -= audiolength;
 
-       if (data[pos])
+       for (int i = 0; i < resy; i++)
        {
-           SDL_RenderCopy(sdlRenderer, send1, NULL, NULL);
+          float val = audiobuff[(pos + i) % audiolength];
+          for (int j = 0; j < resx; j++)
+	  {
+             uint32_t rgb = SDL_MapRGB(screen->format, int(val*carrier[j]), int(val*carrier[j]), int(val*carrier[j]));
+             ((uint32_t*)screen->pixels)[i * resx + j] = rgb;
+	  }
        }
-       else
-       {
-           SDL_RenderCopy(sdlRenderer, send0, NULL, NULL);
-       }
-       SDL_RenderPresent(sdlRenderer);
        
-       pos++;
+       SDL_UpdateTexture(send, NULL, screen->pixels, resx*sizeof(uint32_t));
+       SDL_RenderCopy(sdlRenderer, send, NULL, NULL);
+       SDL_RenderPresent(sdlRenderer);
+       pos += verticalspan;
   };
 };
 
@@ -86,10 +75,11 @@ void usage()
 
 int main(int argc, char *argv[])
 {
-  carrier=10000000.0;
+  fc=10000000.0;
   resx=1024;
   resy=768;
   horizontalspan=1400;
+  verticalspan=1600;
   pixelclock=105.0 * 1e6;
   char *filename;
 
@@ -104,63 +94,47 @@ int main(int argc, char *argv[])
 	 "if you do not read it, you will NOT know what to do\n"
 	 );
 
-  if (argc!=7) usage();
+  if (argc!=8) usage();
   pixelclock=atof(argv[1]);
   resx=atol(argv[2]);
   resy=atol(argv[3]);
   horizontalspan=atol(argv[4]);
-  carrier=atof(argv[5]);
-  filename=argv[6];
+  verticalspan=atol(argv[5]);
+  fc=atof(argv[6]);
+  filename=argv[7];
 
   printf("\n"
 	 "Pixel Clock %.0f Hz\n"
 	 "X Resolution %d Pixels\n"
 	 "Y Resolution %d Pixels\n"
 	 "Horizontal Total %d Pixels\n"
+	 "Vertical Total %d Pixels\n"
 	 "AM Carrier Frequency %.0f Hz\n"
 	 "\n\n",
-	 pixelclock,resx,resy,horizontalspan,carrier);
+	 pixelclock,resx,resy,horizontalspan,verticalspan,fc);
 
   sdlWindow = SDL_CreateWindow("waha", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 0, 0, SDL_WINDOW_FULLSCREEN_DESKTOP);
   sdlRenderer = SDL_CreateRenderer(sdlWindow, -1, SDL_RENDERER_PRESENTVSYNC);
   screen = SDL_CreateRGBSurface(0, resx, resy, 32, 0, 0, 0, 0);
-  send0 = SDL_CreateTexture(sdlRenderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, resx, resy);
-  send1 = SDL_CreateTexture(sdlRenderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, resx, resy);
+  send = SDL_CreateTexture(sdlRenderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, resx, resy);
   SDL_SetRenderDrawColor(sdlRenderer, 0, 0, 0, 255);
   SDL_RenderClear(sdlRenderer);
   SDL_RenderPresent(sdlRenderer);
+
+  carrier = (float*)malloc(sizeof(float) * resx);
+  for (int i = 0; i < resx; i++)
+  {
+     carrier[i] = (sin(2*M_PI*fc/pixelclock*i) + 1) / 2;
+  }
   
+  FILE* audio = fopen(filename, "rb");
+  fseek(audio, 0, SEEK_END);
+  audiolength = ftell(audio);
+  fseek(audio, 0, SEEK_SET);
+  audiobuff = (uint8_t*)malloc(audiolength);
+  fread(audiobuff, audiolength, 1, audio);
 
-  double variance = horizontalspan*resy / 2 / 3;
-  variance *= variance;
-  double mean = horizontalspan*625 / 2;
-
-  for (int posy = 0; posy < resy; posy++)
-  {
-     for (int posx = 0; posx < resx; posx++)
-     {
-        int pos = posy * horizontalspan + posx;
-        int val = cos(2*M_PI*carrier/pixelclock*pos) * 128 + 127;
-        double mask = exp(-pow(pos - mean, 2)/2/variance);
-        ((uint32_t*)screen->pixels)[posy * resx + posx] = SDL_MapRGB(screen->format, int(mask*val), int(mask*val), int(mask*val));
-     }
-  }
-  SDL_UpdateTexture(send1, NULL, screen->pixels, resx * sizeof(uint32_t));
-
-  for (int posy = 0; posy < resy; posy++)
-  {
-     for (int posx = 0; posx < resx; posx++)
-     {
-        int pos = posy * horizontalspan + posx;
-        int val = 0;-cos(2*M_PI*carrier/pixelclock*pos) * 128 + 127;
-        double mask = exp(-pow(pos - mean, 2)/2/variance);
-        ((uint32_t*)screen->pixels)[posy * resx + posx] = SDL_MapRGB(screen->format, int(mask*val), int(mask*val), int(mask*val));
-     }
-  }
-  SDL_UpdateTexture(send0, NULL, screen->pixels, resx * sizeof(uint32_t));
-
-
-  play ();
+  play();
 
   return 0;
 };
