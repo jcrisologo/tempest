7527e676fa115eb66374900ec997708f3bb63d8e
jcrisologo <jcrisologo@users.noreply.github.com>
2019-04-30 23:20:34 -0700

    Forgot header

---
+++ include/monitor_modulator.h
@@ -0,0 +1,55 @@
+#ifndef _MONITOR_MODULATOR_H_
+#define _MONITOR_MODULATOR_H_
+
+typedef enum
+{
+   MOD_MODE_AM,
+   MOD_MODE_OOK,
+   MOD_MODE_BPSK,
+   MOD_MODE_QPSK
+} mod_mode_t;
+
+typedef struct
+{
+   int resx;
+   int resy;
+   int spanx;
+   int spany;
+   double fc;
+   double fs;
+   mod_mode_t mod_mode;
+} monitor_modulator_params_t;
+
+typedef struct
+{
+   monitor_modulator_params_t params;
+   SDL_Window* window;
+   SDL_Renderer* renderer;
+   SDL_Texture* texture;
+   SDL_Surface* surface;
+   pulse_shaper_t* pulse_shaper_i;
+   pulse_shaper_t* pulse_shaper_q;
+   mixer_t* mixer;
+   int data_buffer[2]; // buffers i and q samp for QPSK only
+   int data_buffer_count;
+} monitor_modulator_t;
+
+void monitor_modulator_init(
+      monitor_modulator_t* mm, 
+      monitor_modulator_params_t params);
+
+void monitor_modulator_sdl_init(monitor_modulator_t* mm);
+
+void monitor_modulator_transmit(monitor_modulator_t* mm, int data);
+void monitor_modulator_display(monitor_modulator_t* mm);
+
+int monitor_modulator_ook_map(int data);
+int monitor_modulator_bpsk_map(int data);
+void monitor_modulator_qpsk_map(int* data, int* samp);
+
+void monitor_modulator_am_draw(monitor_modulator_t* mm, int sym);
+void monitor_modulator_ook_draw(monitor_modulator_t* mm, int sym);
+void monitor_modulator_bpsk_draw(monitor_modulator_t* mm, int sym);
+void monitor_modulator_qpsk_draw(monitor_modulator_t* mm, int isym, int qsym);
+
+#endif
